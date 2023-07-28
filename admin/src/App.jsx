f7cf0241c9d7aebd3f09ff51c44eb388fbb31e15
pengrongwei <pengrongwei@ifun.com>
2023-07-28 17:00:48 +0800

    admin/   Arco Design 组件库引入和基本使用

---
+++ admin/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from "@arco-design/web-react";
+// import "@arco-design/web-react/dist/css/arco.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Button type="primary">hello arco</Button>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
