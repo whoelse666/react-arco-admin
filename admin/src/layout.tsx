b1a7712b6c8f736ac79cb3aad2827423670e0a3d
pengrongwei <pengrongwei@ifun.com>
2023-07-28 18:22:24 +0800

    no message

--- admin/src/layout.tsx
+++ admin/src/layout.tsx
@@ -1,16 +1,16 @@
  
-// function getFlattenRoutes() {
-//   const res = [];
-//   function travel(_routes) {
-//     _routes.forEach((route) => {
-//       if (route.key && !route.children) {
-//         route.component = lazyload(() => import(`./pages/${route.key}`));
-//         res.push(route);
-//       } else if (isArray(route.children) && route.children.length) {
-//         travel(route.children);
-//       }
-//     });
-//   }
-//   travel(routes);
-//   return res;
-// }
+function getFlattenRoutes() {
+  const res = [];
+  function travel(_routes) {
+    _routes.forEach((route) => {
+      if (route.key && !route.children) {
+        route.component = lazyload(() => import(`./pages/${route.key}`));
+        res.push(route);
+      } else if (isArray(route.children) && route.children.length) {
+        travel(route.children);
+      }
+    });
+  }
+  travel(routes);
+  return res;
+}
