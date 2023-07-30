e26829c098dcc82093a68f7ef6a80df833049f6d
whoelse <347300979@qq.com>
2023-07-30 23:47:18 +0800

    ### Commit message: usePagination 新增，编辑，删除

---
+++ src/pages/user/components/drawerForm.tsx
@@ -0,0 +1,110 @@
+import React, { useMemo } from 'react';
+import { useState } from 'react';
+import { Drawer, Form, Input, Message, Button } from '@arco-design/web-react';
+import { User } from '../index';
+import useFormContext from '@arco-design/web-react/es/Form/hooks/useContext';
+import { updateUser, addUser } from '@/requests/user';
+type FormProps = {
+  visible: boolean;
+  setVisible: (b: boolean) => void;
+  editedItem: User;
+  callback: (editedItem?: Partial<User>) => void;
+  width?: number | string;
+};
+const name = '用户信息';
+function DrawerForm({
+  visible,
+  setVisible,
+  editedItem,
+  callback,
+  width,
+}: FormProps) {
+  const [form] = Form.useForm();
+
+  form.getFields;
+  const title = useMemo(
+    () => (editedItem._id ? '更新' : '新增'),
+    [editedItem._id]
+  );
+
+  const onSubmit = async () => {
+    console.log('提交表单 :>> ');
+    const item = form.getFieldsValue();
+    if (!editedItem._id) {
+      const res: any = await addUser(item);
+      if (res._id) {
+        Message.success(title + '用户成功');
+        callback && callback(item);
+        setVisible(false);
+      } else {
+        Message.success(title + '用户失败');
+      }
+    } else {
+      const res: any = await updateUser(editedItem._id, {
+        ...editedItem,
+        ...item,
+      });
+      if (res.data.affected == 1) {
+        Message.success(title + '用户成功');
+        callback && callback({ ...editedItem, ...item });
+        setVisible(false);
+      } else {
+        Message.success(title + '用户失败');
+      }
+    }
+  };
+  return (
+    <>
+      <Drawer
+        width={width ?? 380}
+        title={<span>{title + name} </span>}
+        okText={<span>{title} </span>}
+        visible={visible}
+        onOk={onSubmit}
+        onCancel={() => {
+          setVisible(false);
+        }}
+        afterOpen={() => {
+          form.setFieldsValue(editedItem);
+        }}
+        afterClose={() => {
+          form.resetFields();
+        }}
+      >
+        <Form form={form}>
+          <Form.Item
+            label="手机号"
+            field="phoneNumber"
+            rules={[
+              { required: true, message: '手机号是必填项' },
+              { match: /^1[3456789]\d{9}$/, message: '请输入正确的手机号' },
+            ]}
+          >
+            <Input placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            label="用户名称"
+            field="name"
+            rules={[{ required: true, message: '用户名是必填项' }]}
+          >
+            <Input placeholder="请输入用户名" />
+          </Form.Item>
+          <Form.Item label="邮箱" field="email">
+            <Input placeholder="请输入邮箱" />
+          </Form.Item>
+          <Form.Item label="组织" field="organization">
+            <Input placeholder="请输入组织名称" />
+          </Form.Item>
+          <Form.Item label="职位" field="job">
+            <Input placeholder="请输入职位" />
+          </Form.Item>
+          <Form.Item label="个人站点" field="personalWebsite">
+            <Input placeholder="请输入个人站点URL" />
+          </Form.Item>
+        </Form>
+      </Drawer>
+    </>
+  );
+}
+
+export default DrawerForm;
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,118 +1,136 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Space,
   Card,
   Button,
+  Message,
   Typography,
   Popconfirm,
 } from '@arco-design/web-react';
-import service from '@/utils/request';
-import http from '@/utils/http';
 
-const { Title, Text } = Typography;
+import {
+  findAllUser,
+  findUser,
+  deleteUser,
+  updateUser,
+  addUser,
+} from '@/requests/user';
+const { Title } = Typography;
 import { Table, TableColumnProps } from '@arco-design/web-react';
-import { useRequest, usePagination } from 'ahooks';
+import { usePagination } from 'ahooks';
+import DrawerForm from './components/drawerForm';
 
-const columns: TableColumnProps[] = [
-  {
-    title: '手机号',
-    dataIndex: 'phoneNumber',
-  },
-  {
-    title: '用户名称',
-    dataIndex: 'name',
-  },
-  {
-    title: '用户头像',
-    dataIndex: 'avatar',
-    render(value: string) {
-      return <img src={value} width="60" />;
-    },
-  },
-  {
-    title: '邮箱',
-    dataIndex: 'email',
-    placeholder: '-',
-  },
-  {
-    title: '操作',
-    dataIndex: 'operations',
-    render: (_: unknown, record) => (
-      <>
-        <Button
-          type="text"
-          size="small"
-          onClick={() => tableCallback(record, 'edit')}
-        >
-          编辑
-        </Button>
-        <Popconfirm
-          focusLock
-          title="确认删除吗?"
-          okText="确认"
-          cancelText="取消"
-          onOk={() => tableCallback(record, 'delete')}
-        >
-          <Button type="text" size="small">
-            删除
-          </Button>
-        </Popconfirm>
-      </>
-    ),
-  },
-];
-
-const tableCallback = (record: object, type: string) => {
-  console.log('tableCallback :>> ', tableCallback);
-};
-
-// type pageParamsType = {
-//   pageSize: number;
-//   current: number;
-// };
 const getTableData = async () => {
-  const users = await http.get<{ name: string }>('/api/user');
-  console.log('users :>> ', users);
+  // const users: any = await http.get<{ name: string }>('/api/user');
+  const users: any = await findAllUser();
   return { list: users.data, total: users.meta.total };
 };
+
+export const initial = {
+  _id: '',
+  phoneNumber: '',
+  password: '',
+  name: '',
+  avatar: '',
+  email: '',
+  job: '',
+  jobName: '',
+  organization: '',
+  location: '',
+  personalWebsite: '',
+};
+export type User = typeof initial;
+
 export default function User() {
-  const [tableData, setTableData] = useState([]) as any;
-  /*   const [pagination, setPagination] = useState({
-    current: 1,
-    pageSize: 5,
-    total: 0,
-  });
- useEffect(() => {
-    const { list, total } = getTableData(pagination);
-    console.log(list, total);
-    setTableData(list);
-    setPagination({ ...pagination, total });
-  }, [pagination.current, pagination.pageSize]);
-
-  const { data, error, loading } = useRequest(() => getTableData(pagination), {
-    onSuccess: ({ total }) => setPagination({ ...pagination, total }),
-    refreshDeps: [pagination.current, pagination.pageSize],
-  }); */
-
+  const [editedItem, setEditedItem] = useState(initial);
+  const [visible, setVisible] = useState(false);
   const { data, pagination, loading, refresh } = usePagination(getTableData, {
     defaultCurrent: 1,
     defaultPageSize: 2,
   });
 
-  const onChange = (pagination) => {
-    // setPagination(pagination);
+  const delTableItem = async (id) => {
+    if (!id) return;
+    // const res: any = await http.delete('/api/user/' + id);
+    const res: any = await deleteUser(id);
+    if (res.affected === 1) {
+      Message.success('删除成功');
+      refresh();
+    } else {
+      Message.error('删除失败');
+    }
   };
 
+  const tableCallback = async (record, operation: string) => {
+    if (operation == 'edit') {
+      setEditedItem(record);
+      setVisible(true);
+    } else {
+      await delTableItem(record._id);
+    }
+  };
+
+  const columns: TableColumnProps[] = [
+    {
+      title: '手机号',
+      dataIndex: 'phoneNumber',
+    },
+    {
+      title: '用户名称',
+      dataIndex: 'name',
+    },
+    {
+      title: '用户头像',
+      dataIndex: 'avatar',
+      render(value: string) {
+        return <img src={value} width="60" />;
+      },
+    },
+    {
+      title: '邮箱',
+      dataIndex: 'email',
+      placeholder: '-',
+    },
+    {
+      title: '操作',
+      dataIndex: 'operations',
+      render: (_: unknown, record) => (
+        <>
+          <Button
+            type="text"
+            size="small"
+            onClick={() => tableCallback(record, 'edit')}
+          >
+            编辑
+          </Button>
+          <Popconfirm
+            focusLock
+            title="确认删除吗?"
+            okText="确认"
+            cancelText="取消"
+            onOk={() => tableCallback(record, 'delete')}
+          >
+            <Button type="text" size="small">
+              删除
+            </Button>
+          </Popconfirm>
+        </>
+      ),
+    },
+  ];
+  const onAdd = () => {
+    setEditedItem(initial);
+    setVisible(true);
+  };
   return (
     <Card>
       <Title heading={6}>用户管理</Title>
       <Space direction="vertical" style={{ width: '100%' }}>
-        <Button type="primary" style={{ marginBottom: 10 }}>
+        <Button onClick={onAdd} type="primary" style={{ marginBottom: 10 }}>
           新增
         </Button>
         <Table
           loading={loading}
-          onChange={onChange}
           pagination={pagination}
           columns={columns}
           data={data?.list}
@@ -120,6 +138,15 @@ export default function User() {
           style={{ width: '100%' }}
         />
       </Space>
+      <DrawerForm
+        {...{
+          visible,
+          setVisible,
+          editedItem,
+          width: 500,
+          callback: () => refresh(),
+        }}
+      ></DrawerForm>
     </Card>
   );
 }
